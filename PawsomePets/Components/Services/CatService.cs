aa62b593dd407a0d0e4f9e19221b01ac9d587212
sophie <d00259237@student.dkit.ie>
2024-11-30 20:30:34 +0000

    Created DogService and CatService to interact with The Dog API and The Cat API for breed data

--- PawsomePets/Components/Services/CatService.cs
+++ PawsomePets/Components/Services/CatService.cs
@@ -1,6 +1,39 @@
-﻿namespace PawsomePets.Components.Services
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using PawsomePets.Components.Models; 
+
+namespace PawsomePets.Components.Services
 {
     public class CatService
     {
+        private readonly HttpClient _httpClient;
+
+
+        public CatService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+       
+        public async Task<List<CatBreed>> GetBreedsAsync()
+        {
+            
+            var response = await _httpClient.GetStringAsync("breeds");
+
+            
+            return JsonConvert.DeserializeObject<List<CatBreed>>(response);
+        }
+
+        
+        public async Task<List<CatImage>> GetCatImagesAsync()
+        {
+            
+            var response = await _httpClient.GetStringAsync("images/search?limit=5"); 
+
+           
+            return JsonConvert.DeserializeObject<List<CatImage>>(response);
+        }
     }
 }
+
--- PawsomePets/Components/Services/DogService.cs
+++ PawsomePets/Components/Services/DogService.cs
@@ -1,6 +1,24 @@
-﻿namespace PawsomePets.Components.Services
+﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+
+
+namespace PawsomePets.Components.Models;
+
+public class DogService
 {
-    public class DogService
+
+    private HttpClient _httpClient;
+
+    public DogService(HttpClient httpClient)
     {
+        _httpClient = httpClient;
     }
+
+    public async Task<List<Dog>> GetDogsAsync()
+    {
+        var response = await _httpClient.GetStringAsync("breeds");
+        return JsonConvert.DeserializeObject<List<Dog>>(response);
+    }
+
 }
