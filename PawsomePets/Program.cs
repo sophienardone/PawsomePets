6f75c8eb7a8fd5b0ae633a396da8b5b31065426d
sophie <d00259237@student.dkit.ie>
2024-11-30 20:35:07 +0000

    Created HttpClient for Dog and Cat APIs with keys

--- PawsomePets/Program.cs
+++ PawsomePets/Program.cs
@@ -1,4 +1,7 @@
+using PawsomePets.Components.Models;
 using PawsomePets.Components;
+using System.Net.Http;
+using PawsomePets.Components.Services;
 
 namespace PawsomePets
 {
@@ -8,17 +11,32 @@ namespace PawsomePets
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+           
+            builder.Services.AddHttpClient<DogService>(client =>
+            {
+                
+                client.BaseAddress = new Uri("https://api.thedogapi.com/v1/");
+                client.DefaultRequestHeaders.Add("x-api-key", "live_kojvayaJ9xfHlmWbsY9NyOup4ScfwnVS30IhULAGb0wKXU1HgIlM7e8cz56zSKCo"); 
+            });
+
+            builder.Services.AddHttpClient<CatService>(client =>
+            {
+             
+                client.BaseAddress = new Uri("https://api.thecatapi.com/v1/");
+
+                
+                client.DefaultRequestHeaders.Add("x-api-key", "live_WAgvVqgUBZU4IgkB4MLPcomFeNQwjZQxaGldvGTmbRoPrMpTM4IG3mkVyZI63Par");
+            });
+
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
