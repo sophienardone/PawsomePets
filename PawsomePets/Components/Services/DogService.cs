2a2634c2ba4d061f20bd068966a1eb4965fdbef1
sophie <d00259237@student.dkit.ie>
2024-12-01 20:00:22 +0000

    Modified DogService.cs

--- PawsomePets/Components/Services/DogService.cs
+++ PawsomePets/Components/Services/DogService.cs
@@ -1,5 +1,7 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Text.Json.Serialization;
 using Newtonsoft.Json;
+using RestSharp;
 
 
 
@@ -8,17 +10,43 @@ namespace PawsomePets.Components.Models;
 public class DogService
 {
 
-    private HttpClient _httpClient;
+    private readonly HttpClient _httpClient;
+
+    private const string BaseUrl = "https://api.thedogapi.com/v1/";
 
     public DogService(HttpClient httpClient)
     {
+
         _httpClient = httpClient;
+
+        _httpClient.DefaultRequestHeaders.Add("x-api-key", "live_kojvayaJ9xfHlmWbsY9NyOup4ScfwnVS30IhULAGb0wKXU1HgIlM7e8cz56zSKCo");
     }
 
-    public async Task<List<Dog>> GetDogsAsync()
+
+    public async Task<List<DogBreed>> GetDogBreedsAsync()
     {
-        var response = await _httpClient.GetStringAsync("breeds");
-        return JsonConvert.DeserializeObject<List<Dog>>(response);
+        var response = await _httpClient.GetFromJsonAsync<List<DogBreed>>($"{BaseUrl}breeds");
+        if (response != null)
+        {
+            foreach (var breed in response)
+            {
+                if (!string.IsNullOrEmpty(breed.reference_image_id))
+                {
+
+                    breed.Image = new DogImage
+                    {
+                        Url = $"https://cdn2.thedogapi.com/images/{breed.reference_image_id}.jpg",
+                    };
+                }
+            }
+            }
+            return response ?? new List<DogBreed>();
+        }
     }
 
-}
+
+
+
+
+
+
