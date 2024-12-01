da27ac12bca6028d57d33b920c03c95d0e03ece1
sophie <d00259237@student.dkit.ie>
2024-12-01 20:52:17 +0000

    Updated CatService to fetch cat breeds from The Cat API

--- PawsomePets/Components/Services/CatService.cs
+++ PawsomePets/Components/Services/CatService.cs
@@ -1,39 +1,34 @@
 ﻿using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-using PawsomePets.Components.Models; 
+using PawsomePets.Components.Models;
+using RestSharp;
 
 namespace PawsomePets.Components.Services
 {
     public class CatService
     {
         private readonly HttpClient _httpClient;
+        private const string BaseUrl = "https://api.thecatapi.com/v1/";
 
 
         public CatService(HttpClient httpClient)
         {
             _httpClient = httpClient;
+            _httpClient.DefaultRequestHeaders.Add("x-api-key", "live_WAgvVqgUBZU4IgkB4MLPcomFeNQwjZQxaGldvGTmbRoPrMpTM4IG3mkVyZI63Par");
         }
 
-       
+
         public async Task<List<CatBreed>> GetBreedsAsync()
         {
-            
-            var response = await _httpClient.GetStringAsync("breeds");
-
-            
-            return JsonConvert.DeserializeObject<List<CatBreed>>(response);
-        }
-
-        
-        public async Task<List<CatImage>> GetCatImagesAsync()
-        {
-            
-            var response = await _httpClient.GetStringAsync("images/search?limit=5"); 
-
-           
-            return JsonConvert.DeserializeObject<List<CatImage>>(response);
+            var response = await _httpClient.GetFromJsonAsync<List<CatBreed>>($"{BaseUrl}breeds");
+            return response ?? new List<CatBreed>();
         }
     }
 }
+
+
+
+
+
 
