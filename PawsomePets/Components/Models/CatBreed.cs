da31151fbada61bab5209607c090e85cfbcb6b4e
sophie <d00259237@student.dkit.ie>
2024-12-01 20:53:33 +0000

    Modified CatBreed.cs

--- PawsomePets/Components/Models/CatBreed.cs
+++ PawsomePets/Components/Models/CatBreed.cs
@@ -1,12 +1,29 @@
-﻿namespace PawsomePets.Components.Models
+﻿using Newtonsoft.Json;
+namespace PawsomePets.Components.Models
 {
     public class CatBreed
     {
+        [JsonProperty("id")]
         public string Id { get; set; }
+
+        [JsonProperty("name")]
         public string Name { get; set; }
+
+        [JsonProperty("description")]
         public string Description { get; set; }
+
+        [JsonProperty("temperament")]
         public string Temperament { get; set; }
 
-        public string ImageUrl { get; set; }
+        [JsonProperty("life_span")]
+        public string life_Span { get; set; }
+
+        [JsonProperty("origin")]
+        public string Origin { get; set; }
+
+        [JsonProperty("reference_image_id")]
+        public string reference_image_id { get; set; }
+
+       
     }
 }
